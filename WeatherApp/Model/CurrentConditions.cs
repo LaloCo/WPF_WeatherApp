d8fd5c5dbd6c97981d4eb8cc2f5a89e15a7eb1f7
Eduardo Rosas <lalorosas@outlook.com>
2020-09-29 10:59:01 -0500

    Defining the classes

---
+++ WeatherApp/Model/CurrentConditions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Model
+{
+    public class Values
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Values Metric { get; set; }
+        public Values Imperial { get; set; }
+    }
+
+    public class Weather
+    {
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public object PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class CurrentConditions
+    {
+        public List<Weather> Conditions { get; set; }
+    }
+}
